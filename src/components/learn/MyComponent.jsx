09a1798454df60e1bbe393d14ab07e4346f2a44d
Quoc Nguyen <quoc.nguyen@s3corp.com.vn>
2025-07-18 14:33:19 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,13 @@
 import './style.css'
 
 const MyComponent = () => {
+
+    // const bientest = [1, 2, 3];
+    const bientest = { name: "Quoc", age: 29 };
     return (
         <>
-            <div>Hello</div>
+            <div>Hello {JSON.stringify(bientest)}</div>
+            {console.log("Video 31")}
             <div className='child' style={{ fontWeight: "700" }}>Div child</div>
         </>
     )
