b441a4f68373a3a98dc5e84adca793dc7cbbc3fb
Kent Tap Code <quocthenguyen1996@gmail.com>
2025-07-19 22:33:06 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,18 @@
 const TodoNew = (props) => {
     console.log(props)
     const { AddNewTodo } = props;
-    AddNewTodo('Quoc')
+    // AddNewTodo('Quoc')
+    const handleClick = () => {
+        alert('click')
+    }
+
+    const hanldeOnChange = (name) => {
+        console.log("gia tri: ", name)
+    }
     return (
         <div className='todo-new'>
-            <input type='text' />
-            <button>Add</button>
+            <input type='text' onChange={(e) => hanldeOnChange(e.target.value)} />
+            <button onClick={handleClick}>Add</button>
         </div>
     )
 }
