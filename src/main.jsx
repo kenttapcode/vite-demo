7a6426c6067d7cd03f2b880ef99fe62f22201ead
Kent Tap Code <quocthenguyen1996@gmail.com>
2025-07-20 14:21:25 +0700

    video 45

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login Page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Register Page</div>,
+  },
+  {
+    path: "/product",
+    element: <div>Product Page</div>,
+  },
+  {
+    path: "/users",
+    element: <div>Users Page</div>,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
